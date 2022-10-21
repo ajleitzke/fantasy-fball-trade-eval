5320551566c30a5b8791c012e5a6b3ec661973d9
Alexander Leitzke <78230669+ajleitzke@users.noreply.github.com>
2022-10-21 08:53:09 -0500

    Added exceptions/new window

--- tradeeval.py
+++ tradeeval.py
@@ -13,11 +13,25 @@ def calcValue(*args):
     t1val = 0
     t2val = 0
     for i in t1:
-        a = df.loc[i, 'Value']
-        t1val += a
+        try:
+            a = df.loc[i, 'Value']
+            t1val += a
+        except KeyError:
+            newWindow1 = Toplevel(root)
+            newWindow1.title("Error!")
+            err1 = ttk.Label(newWindow1, text="Error! Player not found!\n(check your 'giving away' entry for spelling)")
+            err1.grid(column=1,row=1)
+            return
     for j in t2:
-        b = df.loc[j, 'Value']
-        t2val += b
+        try:
+            b = df.loc[j, 'Value']
+            t2val += b
+        except KeyError:
+            newWindow2 = Toplevel(root)
+            newWindow2.title("Error!")
+            err2 = ttk.Label(newWindow2, text="Error! Player not found!\n(check your 'receiving' entry for spelling)")
+            err2.grid(column=1,row=1)
+            return
     if t1val == t2val:
         res = ttk.Label(mainframe, text='Giving Up: ' + str(t1val) + '\nReceiving: ' + str(t2val) + "\nThis trade is fair!")
         res.grid(column=1,row=7)
@@ -34,7 +48,7 @@ mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 t1 = StringVar()
-welcome = ttk.Label(mainframe, text="Welcome to my trade evaluator!", font=(18))
+welcome = ttk.Label(mainframe, text="Welcome to my trade evaluator!", font=(BOLD)
 welcome.grid(column = 1, row = 1)
 t1label = ttk.Label(mainframe, text="Enter players to trade away...\n(use commas to seperate multiple players)")
 t1label.grid(column = 1, row = 2)
@@ -47,4 +61,6 @@ t2entry = ttk.Entry(mainframe, textvariable = t2)
 t2entry.grid(column=1,row=5)
 submit = ttk.Button(mainframe, text="Submit", command=calcValue)
 submit.grid(column=1,row=6)
-root.mainloop()
+credits = ttk.Label(mainframe, text="Created by Alexander Leitzke | github.com/ajleitzke")
+credits.grid(column=1,row=8)
+mainloop()
